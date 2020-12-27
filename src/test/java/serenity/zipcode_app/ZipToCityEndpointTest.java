4ccf0e0adedd79e3e4a10bc552604704af4317e7
JHY34 <hamityilmaz34@gmail.com>
2020-12-27 11:20:41 -0600

    zipcode parameterized test practice with Serenity

--- src/test/java/serenity/zipcode_app/ZipToCityEndpointTest.java
+++ src/test/java/serenity/zipcode_app/ZipToCityEndpointTest.java
@@ -2,12 +2,18 @@ package serenity.zipcode_app;
 
 import io.restassured.RestAssured;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static net.serenitybdd.rest.SerenityRest.*;
+
+import static org.hamcrest.Matchers.*;
 
 @SerenityTest
 public class ZipToCityEndpointTest {
@@ -31,9 +37,36 @@ public class ZipToCityEndpointTest {
                 .pathParam("country","us")
                 .pathParam("zipcode","22030").
         when()
-                .get("/{country}/{zipcode}").prettyPeek()
+                .get("/{country}/{zipcode}").
+        then()
+                .statusCode(200)
+                .body("'post code'" , is ("22030"))
+                .body("places[0].'place name'" , is("Fairfax"))
                 ;
     }
+
+    @DisplayName("Testing multiple zipcodes and get the result")
+    @ParameterizedTest
+    @ValueSource(strings = {"22030", "22031", "22032", "22034", "22035"})
+    public void testZipCodes(String zip){
+
+        //run this parameterized test with 5 zipcodes of your choice
+        // start with no external file
+        // then add external csv file in separate test
+
+        System.out.println("zip = " + zip);
+        given()
+                .pathParam("country","us")
+                .pathParam("zipcode","22030").
+        when()
+                .get("/{country}/{zipcode}");
+
+        Ensure.that("We got successful result " , v->v.statusCode(200));
+
+
+    }
+
+
 
 
 
